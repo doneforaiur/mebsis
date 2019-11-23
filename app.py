14f9dce208bb5fb44bd6b2c288afd1e6368e7fdf
Mirza Atlı <mirza.a.96@hotmail.com>
2019-11-23 15:27:51 +0300

    mesaj ve takipçiler/takipedilenler

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python/
+#!/usr/bin/python3/
 #-*-coding:utf-8-*-
 
 
-
-from flask import Flask, request, render_template, make_response
+from flask import Flask, request, url_for, render_template, make_response, redirect
 import MySQLdb
 import hashlib
 app = Flask(__name__)
@@ -12,7 +11,8 @@ app = Flask(__name__)
 
 @app.route('/index.html',methods=['GET'])
 @app.route('/',methods=['GET'])
-def db():
+def index():
+	
     db = MySQLdb.connect("localhost","misafir","misafir","mebsis",charset='utf8', init_command='SET NAMES UTF8' )
     cursor = db.cursor()
     cursor.execute("SELECT count(*) from mezun")
@@ -27,12 +27,14 @@ def contact():
     return render_template('contact.html')
 
 
+
 @app.route('/mezun_ara', methods=['GET', 'POST'])
 def show_post():
     if request.method == 'POST':
         ogrenci_no1 = str(request.form['ogrenci_no'])
         page_number = str(request.form['page_number'])
         page_number=int(page_number)-1
+        
     if request.method == 'GET':
         ogrenci_no1=0
         page_number=0
@@ -63,7 +65,7 @@ def show_post():
     return render_template('elements.html', data=rows, len=len(rows), page_number=page_number,current_page=current_page,
  low_limit=low_limit, high_limit=high_limit)
 
-@app.route('/login', methods = ['POST'])
+@app.route('/login', methods = ['GET','POST'])
 def login():
     error = ''
     # Error ekle.
@@ -75,16 +77,80 @@ def login():
     result = cursor.fetchone()
 
     if(str(result[0]) == hashlib.md5(password.encode()).hexdigest()):
-        response =  make_response(render_template('index.html'))
+        response = redirect(url_for('anasayfa'))
         response.set_cookie('username', username)
         response.set_cookie('password', str(result[0]))
     return response
 
+@app.route('/cikis_yap', methods= ['POST'])
+def cikis_yap():
+    resp = redirect(url_for('index'))
+    resp.set_cookie('username', expires=0)
+    resp.set_cookie('password', expires=0)
+    return resp
+
 @app.route('/anasayfa')
 def anasayfa():
-    return
+    db = MySQLdb.connect("localhost","misafir","misafir","mebsis",charset='utf8', init_command='SET NAMES UTF8' )
+    cursor = db.cursor()
+    cursor.execute("SELECT count(*) from mezun")
+    mezun_sayisi = cursor.fetchone()
+    cursor.execute("SELECT count(*) from firma")
+    firma_sayisi = cursor.fetchone()
+    username = request.cookies.get('username')
+    cursor.execute("SELECT isim from mezun where ogrenci_no=" +username+";")
+    username = cursor.fetchone()
+    username = username[0].capitalize().decode('utf8')
+    print(username)
+    return render_template('anasayfa.html',username=username,mezun=mezun_sayisi[0], firma=firma_sayisi[0])
 
-##  Anasayfaya yönlendirecek. Başkalarının ilanı görülecek. İlanları ekle. İlan yorumları, özel mesajlar. bitti gitti
+@app.route('/profil', methods=['GET', 'POST'])
+def profil():
+	ogrenci_no = request.args.get('ogrenci_no')
+	db = MySQLdb.connect("localhost","misafir","misafir","mebsis")
+	cursor = db.cursor()
+	cursor.execute("SELECT * from mezun where ogrenci_no=" +str(ogrenci_no)+";")
+	ogrenci = cursor.fetchone()
+	
+	cursor.execute("SELECT unvan from firma where ticari_sicil='"+str(ogrenci[10])+"';")
+	firma = cursor.fetchone()
+	firma = firma[0]
+	isim = ogrenci[1].capitalize()
+	soyad = ogrenci[2].capitalize()
+	return render_template('profil.html', ogrenci_no=ogrenci_no, isim=isim.decode('latin1'), soyad=soyad.decode('latin1'), firma=firma)
+
+@app.route('/takipciler', methods=['GET', 'POST'])
+def takipciler():
+	ogrenci_no = request.args.get('ogrenci_no')
+	db = MySQLdb.connect("localhost","misafir","misafir","mebsis",charset='utf8', init_command='SET NAMES UTF8')
+	cursor = db.cursor()
+	cursor.execute("SELECT ogrenci_no, isim, soyad from mezun where ogrenci_no IN (SELECT follower_id FROM takip WHERE following_id='" +str(ogrenci_no)+"');")
+	result = cursor.fetchall()
+	return render_template('elements.html', data=result, len=len(result), low_limit=0, high_limit=5)
+
+
+@app.route('/takipedilenler', methods=['GET', 'POST'])
+def takipedilenler():
+	ogrenci_no = request.args.get('ogrenci_no')
+	db = MySQLdb.connect("localhost","misafir","misafir","mebsis",charset='utf8', init_command='SET NAMES UTF8')
+	cursor = db.cursor()
+	cursor.execute("SELECT ogrenci_no, isim, soyad from mezun where ogrenci_no IN (SELECT following_id FROM takip WHERE follower_id='" +str(ogrenci_no)+"');")
+	result = cursor.fetchall()
+	return render_template('elements.html', data=result, len=len(result), low_limit=0, high_limit=5)
+
+@app.route('/mesaj', methods = ['GET', 'POST'])
+def mesaj():
+	username = request.cookies.get('username')
+	message_to = request.args.get('message_to')	
+	db = MySQLdb.connect("localhost","misafir","misafir","mebsis",charset='utf8', init_command='SET NAMES UTF8')
+	cursor = db.cursor()
+	cursor.execute("SELECT mesaji_atan, mesaj, mesaj_tarihi from mesaj where konusma_id_fk IN (SELECT konusma_id FROM konusma where (kullanici_bir='"+str(username)+"' OR kullanici_bir='"+str(message_to)+"') AND (kullanici_iki='"+str(message_to)+"' OR kullanici_iki='"+str(username)+"'));")
+	mesaj_data = cursor.fetchall() # Temizlenmesi gerek.
+	print(mesaj_data)
+	return render_template('mesaj.html', data=mesaj_data, len=len(mesaj_data), username=username, message_to=message_to)
+
+
+## Başkalarının ilanı görülecek. İlanları ekle. İlan yorumları, özel mesajlar. bitti gitti
 
 
 if __name__ == '__main__':
